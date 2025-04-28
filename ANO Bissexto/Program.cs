4b05ed17b47706754718264d2147e0047b918c70
Gu1x <guilhermesantana922@gmail.com>
2025-04-28 12:43:51 -0300

    Add files via upload

---
+++ ANO Bissexto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANO_Bissexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano;
+            Console.Write("digite o ano: ");
+            ano = int.Parse(Console.ReadLine());
+            
+
+            
+           if (ano%400==0 || (ano % 4 == 0 && ano % 100 != 0)) 
+            {
+                Console.WriteLine("esse ano é bissexto");
+            }
+            else
+            {
+                Console.WriteLine("esse ano não é bissexto");
+            }
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ Aposentadoria/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aposentadoria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, anoT;
+
+            Console.WriteLine("digite sua idade e seus anos trabalhados para saber se você pode ou não se aposentar");
+            Console.Write("digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+            Console.Write("digite seus anos trabalhados: ");
+            anoT = int.Parse(Console.ReadLine());
+
+            if (idade > 64 || anoT > 29)
+            {
+                Console.WriteLine("você já pode se aposentar");
+            }
+            else if (idade >=60 && anoT >= 25)
+            {
+                Console.WriteLine("você já pode se aposentar");
+            }
+            else
+            {
+                Console.WriteLine("você não pode se aposentar");
+            }
+            Console.ReadKey();
+        }
+    }
+}
